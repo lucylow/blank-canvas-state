8b2ee686e909a080fc86b0d0b751c58e8f9d4147
づ｡◕‿◕｡)づ lucylow <llow@edu.uwaterloo.ca>
2025-10-03 10:44:28 -0400

    .

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,8 @@
+import React from "react";
+import Dashboard from "@/components/Dashboard";
+
 const Index = () => {
-  return (
-    <div className="min-h-screen bg-white" />
-  );
+  return <Dashboard />;
 };
 
 export default Index;
